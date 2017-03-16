7ae5a42e9f0a1c62e399686e410833f4958b3561
Rafael Martins <rafael.martins@bemobi.com.br>
2017-03-16 17:03:23 -0300

    introduce GoConvey

--- go/codec/encoder_test.go
+++ go/codec/encoder_test.go
@@ -1,7 +1,20 @@
 package codec
 
-import "testing"
+import (
+	"testing"
 
-func TestEncode1(t *testing.T) {
-	t.Error("falhou tamoyo")
+	"github.com/bemobi/goconvey/convey"
+	"github.com/xor-gate/bjf"
+)
+
+func TestEncode(t *testing.T) {
+	convey.Convey("Dado que eu passo o numero 1", t, func() {
+		number := "1"
+		convey.Convey("Quando eu chamo o Encode", func() {
+			got := bjf.Encode(number)
+			convey.Convey("Entao eu obtenho b", func() {
+				convey.So(got, convey.ShouldEqual, "b")
+			})
+		})
+	})
 }
